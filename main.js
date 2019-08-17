e31883e69bd65fa96f900f4fa2cc6b807f77a2ef
krboelter <krboelter@gmail.com>
2019-08-16 22:06:44 -0600

    set up the project

---
+++ main.js
@@ -0,0 +1,13 @@
+let container = document.querySelector('.container');
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+
+canvas.width = 900;
+canvas.height = 600;
+
+
+let isDrawing;
+
+function draw() {
+    
+}
