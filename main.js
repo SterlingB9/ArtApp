02d33890109c25651b08a4de945667f82532401e
krboelter <krboelter@gmail.com>
2019-08-18 16:33:42 -0600

    make window full screen, banner on top with tools

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-let container = document.querySelector('.container');
-let canvas = document.querySelector('#canvas');
-let ctx = canvas.getContext('2d');
+window.addEventListener('load', () => {
+    let canvas = document.querySelector('#canvas');
+    let ctx = canvas.getContext('2d');
 
-canvas.width = 900;
-canvas.height = 600;
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    })
 
-
-let isDrawing;
-
-function draw() {
+    let isDrawing = true;
+    let x = event.clientX - canvas.getBoundingClientRect().left;
+    let y = event.clientY - canvas.getBoundingClientRect().top;
     
+    function startPosition() {
+        isDrawing = true;
+    }
+
+    function finishPosition() {
+        isDrawing = false;
+    }
+
+    function draw(event) {
+        if (!isDrawing) {
+            return
+        }
+        else {
+            ctx.lineWidth = 5;
+            ctx.lineCap = 'round';
+
+            ctx.lineTo(x, y)
+            ctx.stroke()
+        }
+    }
+
+    canvas.addEventListener('mousedown', startPosition)
+    canvas.addEventListener('mouseup', finishPosition)
+    canvas.addEventListener('mousemove', draw)
+})
+
+function getCoords(event) {
+    console.log(event.clientX, event.clientY)
 }
+
+window.addEventListener('click', getCoords)
